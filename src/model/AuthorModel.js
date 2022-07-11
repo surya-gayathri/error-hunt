df67085bea4d3f594ba9da51a219e42f7ce4a85f
Thejus Rajendran <suryagayathri205@gmail.com>
2022-07-11 20:26:18 +0530

    fixed bugs

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/Library", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
